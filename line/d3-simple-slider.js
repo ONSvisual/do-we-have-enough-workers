f39c272612ebaf1eb3606dc0fb063908d82f99ac
henryjameslau <henry.james.lau@gmail.com>
2019-04-05 09:02:15 +0100

    rollback blue box

--- line/d3-simple-slider.js
+++ line/d3-simple-slider.js
@@ -244,16 +244,16 @@
         .attr('fill', 'white')
         .attr('stroke', '#777');
 
-      handleEnter
-        .append('path')
-        .attr('transform', 'translate(0 24)')
-        .attr('d', d3.symbol()
-          .type(d3.symbolTriangle)
-          .size(150))
-        .style('fill', '#206095')
-        .style('stroke',"none")
+      // handleEnter
+      //   .append('path')
+      //   .attr('transform', 'translate(0 24)')
+      //   .attr('d', d3.symbol()
+      //     .type(d3.symbolTriangle)
+      //     .size(150))
+      //   .style('fill', '#206095')
+      //   .style('stroke',"none")
 
-      if (displayValue && value.length === 1) {
+     if (displayValue && value.length === 1) {
         handleEnter
           .append('text')
           .attr('font-size', 10) // TODO: Remove coupling to font-size in d3-axis
@@ -269,27 +269,27 @@
           .text(tickFormat(value[0]));
 
 
-          var text=handleEnter.select('text')
-
-          if(text._groups.length>0){
-            var bbox = text.node().getBBox();
-            handleEnter.select('text').remove()
-            var padding = 5;
-            var rect = handleEnter.append("rect")
-              .attr("x", bbox.x - padding)
-              .attr("y", bbox.y - padding)
-              .attr("ry",5)
-              .attr("width", bbox.width + (padding*2))
-              .attr("height", bbox.height + (padding*2))
-              .style("fill", "#206095");
-            handleEnter
-              .append('text')
-              .text(tickFormat(value[0]))
-              .attr('font-weight',700)
-              .style('fill','white')
-              .attr('y',text.attr('y'))
-              .attr('dy',text.attr('dy'))
-          }
+          // var text=handleEnter.select('text')
+          //
+          // if(text._groups.length>0){
+          //   var bbox = text.node().getBBox();
+          //   handleEnter.select('text').remove()
+          //   var padding = 5;
+          //   var rect = handleEnter.append("rect")
+          //     .attr("x", bbox.x - padding)
+          //     .attr("y", bbox.y - padding)
+          //     .attr("ry",5)
+          //     .attr("width", bbox.width + (padding*2))
+          //     .attr("height", bbox.height + (padding*2))
+          //     .style("fill", "#206095");
+            // handleEnter
+            //   .append('text')
+            //   .text(tickFormat(value[0]))
+              // .attr('font-weight',700)
+              // .style('fill','white')
+              // .attr('y',text.attr('y'))
+              // .attr('dy',text.attr('dy'))
+          // }
 
 
       }
